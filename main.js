0ad03e05a9f7b51fe18e4b2b4778a4b8b700e09a
ViTrans <tran.vi487@gmail.com>
2021-12-13 15:29:56 +0700

    upload source code

---
+++ main.js
@@ -0,0 +1,38 @@
+
+
+// create 1 ham de tim 
+const qall = (str) => {
+    return document.querySelectorAll(str)
+}
+
+const q = (str) => {
+    return document.querySelector(str)
+}
+// tim class input check box
+const inputs = qall('.input-checkbox')
+console.log(inputs)
+
+for (let i = 0; i < inputs.length; i++) {
+    const inputcheckbox = inputs[i];
+    inputcheckbox.addEventListener("change",(e) =>{
+        const target = e.target;
+        const checked = target.checked;
+        // console.dir(e.target.checked)
+        // console.log(target.dataset)
+        const data = target.dataset
+        // console.log(data.id)
+        const content = q('.content')
+        console.log(content)
+        console.log('checked',checked)
+        content.style.textDecoration = 'line-through'
+        if(checked){
+            content.style.textDecoration = 'line-through'
+        }
+        else{
+            content.style.textDecoration = 'none'
+        }
+
+
+    })
+    
+}
